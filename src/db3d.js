2b71dc54681b85865390f18fdb7fd87d3fc0e6e8
atlee19 <atleegraham16@gmail.com>
2021-03-07 16:56:22 -0500

    top level structure done

---
+++ src/db3d.js
@@ -0,0 +1,14 @@
+var db3d = {}
+db3d.version = 0.0.1;
+// db3d.scanner = scanner;
+// db3d.parser = parser;
+// db3d.transformer = transformer;
+// db3d.codegen = codegen;
+
+// db3d.transpile = function(code){
+
+// }
+
+// const code = 'Green Cube Size 1 Position 0 0 -1';
+// const db3d = db3d.transpile(code);
+//document append script
