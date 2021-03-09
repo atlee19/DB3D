3956e379e1e8a0bfb41cc876b8b59d6774ccdc96
atlee19 <atleegraham16@gmail.com>
2021-03-09 10:40:24 -0500

    added more test cases

--- src/db3d.js
+++ src/db3d.js
@@ -12,7 +12,13 @@ db3d.transpile = function(code){
     this.scanner(code);
 }
 
-// const code = 'Green Cube Size 1 Position 0 0 -1';
-const code = 'sdaa Cube Size 1 Position 0 0 -1';
+//good code
+const code = 'Green Cube Size 1 Position 0 0 -1'; 
+
+//spelling mistake
+// const code = 'Green Cube Size 1 Postion 0 0 -1'
+
+//unrecognized word
+// const code = 'sdaa Cube Size 1 Position 0 0 -1';
 const transpiled_result = db3d.transpile(code);
 //document append script
